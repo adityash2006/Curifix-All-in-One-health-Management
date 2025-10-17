4a1adad31da1b6fcfc5a4d2b3dc56147b12838fd
Khushbu <guptakhushu2106@gmail.com>
2025-10-17 14:04:50 +0530

    Added some style to make side responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,9 +23,9 @@ export default function Home() {
     return (<>
     <Navbar></Navbar>
         <div className=" flex flex-col items-center pt-25  min-h-screen/2 bg-white">
-            <h1 className="instrument text-7xl font-bold tracking-tighter text-[#303030] mb-4">Your Personal AI </h1>
-                        <h1 className="instrument text-7xl font-bold tracking-tighter inline text-[#303030] "> Health Companion<span className="text-7xl italic px-4 rounded-lg bg-[rgb(233,255,91)] ml-4">Curifix</span></h1>
-<span className="mt-10 md:w-[35%] text-center text-[#999999]">
+            <h1 className="instrument text-4xl sm:text-5xl lg:text-7xl font-bold tracking-tight inline text-[#303030] text-center">Your Personal AI </h1>
+                        <h1 className="iinstrument text-4xl sm:text-5xl lg:text-7xl font-bold tracking-tighter inline text-[#303030] text-center"> Health Companion<span className="text-4xl sm:text-5xl lg:text-7xl italic px-2 sm:px-4 rounded-lg bg-[rgb(233,255,91)] ml-2 sm:ml-4 whitespace-nowrap">Curifix</span></h1>
+<span className="mt-8 md:mt-10 w-full max-w-lg md:w-[35%] text-center text-[#999999] px-2 text-sm sm:text-base">
            From tracking symptoms to storing reports and finding nearby clinics, Curifix keeps your healthcare simple, smart, and accessible </span>
           <div className="space-x-5 mb-20"> <button class="mt-10 bg-[#a6Ff8D] text-black font-semibold px-6 py-2 
   rounded-[15px] border-solid border-[#BEcf4c] 
@@ -41,16 +41,16 @@ export default function Home() {
 </div>
 
 </div> 
-<div className="flex justify-between">
+<div className="flex justify-center items-end px-2 sm:px-4 mb-20 md:mb-32">
   <div></div>
-  <div className="w-150 ">
+  <div className="hidden md:block w-1/4 max-w-[w-150px] lg:max-w-[200px] self-start translate-y-[-20px] md-translate-y-[-50px] lg:translate-y-[-80px] mr-[-10px]">
     <img src="injection.png" className="" alt="Injection" />
 
   </div>
-  <div className="">
-  <img src="dashboard.png" className="w-85 md:w-250" alt="Dashboard" />
+  <div className="w-full max-w-3xl">
+  <img src="dashboard.png" className="w-full h-auto object-contain" alt="Dashboard" />
   </div>
-  <div className="w-150">
+  <div className="hidden md:block w-1/4 max-w-[150px] lg:max-w-[200px] self-start translate-y-[-20px] md:translate-y-[-50px] lg:translate-y-[-80px] ml-[-10px]">
     <img src="stethoscope.png" className="" alt="Stethoscope" />
   </div>
 </div>
@@ -61,8 +61,8 @@ export default function Home() {
         
     <CheckDiseaseCard></CheckDiseaseCard>
     <VideoCallCard></VideoCallCard>
-    <div className="lg:ml-42 instrument flex flex-col items-center justify-center min-h-screen/2 bg-white p-6">
-     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 w-full max-w-6xl">
+    <div className="instrument flex flex-col items-center justify-center min-h-screen/2 bg-white p-6">
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 w-full max-w-3xl">
         {/* Emergency Call Card */}
         <StyledCard
           title="Emergency Call"
