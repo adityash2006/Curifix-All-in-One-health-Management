56727a3b3f04fb039618dc6a934ccdbfca2ca153
unknown <adityadgr29@gmail.com>
2026-02-21 00:33:01 +0530

    added / route for backend deployemnet check

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,12 @@ app.post("/chat",async(req,res)=>{
   res.send(b);
 });
 
+app.get("/",(req,res)=>{
+  res.json({
+    message:"server is working "
+  })
+})
+
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
   Connectmongo();
