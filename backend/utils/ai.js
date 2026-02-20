e186bfd614de99ff3e4752944ab5b96e5199c1f1
unknown <adityadgr29@gmail.com>
2026-02-21 02:09:58 +0530

    changed gemini model

--- backend/utils/ai.js
+++ backend/utils/ai.js
@@ -4,7 +4,7 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const llm = new ChatGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_API_KEY,
-  model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash",
   temperature: 0
 });
 
